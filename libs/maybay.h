852d3acf3cef487330a18249179b791fc44eb0c8
thuong <thuong5so55555@gmail.com>
2023-02-21 23:58:56 -0800

    add map, .ignore, maybay.h

--- libs/maybay.h
+++ libs/maybay.h
@@ -1,23 +1,82 @@
 #pragma once
 #include "header.h"
-
-struct MayBay
+const int MAXMB = 300;
+#define MAXMB 300;
+class MAYBAY #@brief MayBay;@note abc;
 {
-    char SoHieuMB[15];
-    char LoaiMB[40];
-    int soDay;
-    int soDong;
+private:
+    char SOHIEUMB[16];
+    char LOAIMB[41];
+    int SODAY;
+    int SODONG;
+public:
+    MAYBAY(char SOHIEUMB[16],char LOAIMB[41], int SODAY,int SODONG);
+    ~MayBay();
 };
 
+MAYBAY::MAYBAY(char SOHIEUMB[16],char LOAIMB[41], int SODAY,int SODONG)
+{
+    this->SOHIEUMB = SOHIEUMB; 
+    this->LOAIMB = LOAIMB;
+    this->SODAY = SODAY;
+    this->SODONG = SODONG;
+}
+
+
+
+MAYBAY::void setSOHIEUMB(char SOHIEUMB)
+{
+    this->SOHIEUMB=SOHIEUMB;
+}
+MAYBAY::char getSOHIEUMB()
+{
+    return SOHIEUMB;
+}
+MAYBAY::void setLOAIMB(char LOAIMB)
+{
+    this->LOAIMB=LOAIMB;
+}
+MAYBAY::char getLOAIMB()
+{
+    return LOAIMB;
+}
+MAYBAY::void setSODAY(int SODAY)
+{
+    this->SODAY=SODAY;
+}
+MAYBAY::int getSODAY()
+{
+    return SODAY;
+}
+MAYBAY::void setSODONG(int SODONG)
+{
+    this->SODONG=SODONG;
+}
+MAYBAY::int getSODONG()
+{
+    return SODONG;
+}
+
+
+
+
+
+{
+}
+
+typedef struct MAYBAY MB;
 struct listMB
 {
     int n;
     MayBay *nodes[MAXMB];
 };
 typedef struct listMB LISTMB;
+<<<<<<< HEAD
+=======
 
 struct Ve
 {
     char ViTriNgoi[5];
     char CMND[11];
 };
+>>>>>>> 1b529a247d788913221165da0f226152d3386c9c
