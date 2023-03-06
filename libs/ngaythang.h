9ea154a84447dea1036f8b71a090e2183bddff69
thuong <thuong5so55555@gmail.com>
2023-03-06 17:14:53 +0700

    Cập nhật file ngaythang

--- libs/ngaythang.h
+++ libs/ngaythang.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "header.h"
 
+#ifndef NGAYTHANG
+#define NGAYTHANG
+
 class Date
 {
 private:
@@ -14,9 +17,15 @@ public:
     Date();
     Date(int, int, int, int, int);
     void setGio(int, int);
+    int getGio();
     void setNgay(int, int, int);
+    int getNgay();
     void showTG();
+    bool checkNgay(int ,int,int);
+    bool checkGio(int,int);
 };
+
+#endif
 
 // Date::Date() {}
 
