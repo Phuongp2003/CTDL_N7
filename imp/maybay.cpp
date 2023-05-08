bc6ceb31645905c98f070c8b331e79bccd44b0ba
thuong <thuong5so55555@gmail.com>
2023-05-08 23:34:15 +0700

    thay doi

--- imp/maybay.cpp
+++ imp/maybay.cpp
@@ -103,6 +103,8 @@ void DsMayBay::insertMB(MayBay *maybay)
 }
 void DsMayBay::deleteMB(int index)
 {
+  if (isEmpty() == true)
+    return;
   delete data[index];
   for (int i = index; i < this->size - 1; i++)
   {
@@ -132,25 +134,6 @@ MayBay *DsMayBay::findMB(const char *soHieuMB)
   }
   return NULL;
 }
-
-// khi tìm và lấy cả danh sách máy bay thì stringsearch không được là == ""
-// DsMayBay DsMayBay::findDsMB(const char *String_Search)
-// {
-//   DsMayBay *dsmb = new DsMayBay();
-//   for (int j = 0; j < size; j++)
-//   {
-//     if (isGotStr(data[j]->getSoHieuMB(), String_Search))
-//     {
-//       dsmb->insertMB(getMB(j));
-//     }
-//   }
-//   if (dsmb->getSize() == 0)
-//   {
-//     delete dsmb;
-//     return *(new DsMayBay());
-//   }
-//   return *dsmb;
-// }
 
 void DsMayBay::deleteDsMB()
 {
